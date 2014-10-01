337a15394d48e4b64c4e161915cf228e1e648e30
kikimans <kikimans@gmail.com>
2014-10-02 00:17:50 +0900

    join post add

--- routes/join.js
+++ routes/join.js
@@ -5,8 +5,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-    res.render('join-form', { title: 'Express' });
-});
+router
+    .get('/', function(req, res) {
+        res.render('join-form', { title: 'Express' });
+    })
+    .post('/', function(req, res){
+        res.render('join-result',
+            {
+                title: 'Express',
+                username : req.body.name,
+                email : req.body.email
+            });
+    });
 
 module.exports = router;
